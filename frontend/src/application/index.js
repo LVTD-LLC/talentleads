dd4047a0d95fc78f1f8c54cb3898f8aa13d5cf69
Rasul Kireev <me@rasulkireev.com>
2026-05-29 12:25:01 +0200

    fix: address Greptile review feedback

--- frontend/src/application/index.js
+++ frontend/src/application/index.js
@@ -5,11 +5,40 @@ import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
 import Dropdown from 'stimulus-dropdown';
 import Reveal from 'stimulus-reveal-controller';
 
+class AccessibleDropdown extends Dropdown {
+  static targets = ['menu', 'button'];
+
+  connect() {
+    super.connect();
+    this.updateExpanded();
+  }
+
+  toggle(event) {
+    super.toggle(event);
+    requestAnimationFrame(() => this.updateExpanded());
+  }
+
+  hide(event) {
+    super.hide(event);
+    requestAnimationFrame(() => this.updateExpanded());
+  }
+
+  updateExpanded() {
+    if (!this.hasButtonTarget) {
+      return;
+    }
+
+    const expanded = !this.menuTarget.classList.contains('hidden');
+    this.buttonTargets.forEach((button) => {
+      button.setAttribute('aria-expanded', String(expanded));
+    });
+  }
+}
 
 // Stimulus
 const application = Application.start();
 const context = require.context("../controllers", true, /\.js$/);
 application.load(definitionsFromContext(context));
 
-application.register('dropdown', Dropdown);
+application.register('dropdown', AccessibleDropdown);
 application.register('reveal', Reveal);
